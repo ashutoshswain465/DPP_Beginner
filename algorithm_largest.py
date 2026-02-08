088d60d7379463937c16e0ad6ea1ca0c2d60294e
ASHUTOSH SWAIN <ashutoshswain465@gmail.com>
2026-02-08 17:46:41 +0530

    Added code to find the maximum of three numbers

---
+++ algorithm_largest.py
@@ -0,0 +1,10 @@
+num1 = 12
+num2 = 45
+num3 = 32
+
+if num1 > num2 and num1 > num3:
+    print(f"The largest number is {num1}")
+elif num2 > num1 and num2 > num3:
+    print(f"The largest number is {num2}")
+else:
+    print(f"The largest number is {num3}")
