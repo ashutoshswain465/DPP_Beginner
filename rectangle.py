d7b06c937aafb52196f5129a99b22a2725c56c98
ASHUTOSH SWAIN <ashutoshswain465@gmail.com>
2026-02-14 19:32:28 +0530

    Added code that calculates rectangle area and perimeter.

---
+++ rectangle.py
@@ -0,0 +1,11 @@
+def rectangle_area(width, height):
+    return width * height
+
+
+def rectangle_perimeter(width, height):
+    return 2 * (width + height)
+
+
+if __name__ == "__main__":
+    print(rectangle_area(10, 3))
+    print(rectangle_perimeter(10, 3))
